7cb8cd1e47bdc6f758dba053a2328b9af6cd2e5a
michkail <pitermichkail@ymail.com>
2021-12-17 17:09:21 +0700

    hmm

--- apiebooks/ebooks/api/views.py
+++ apiebooks/ebooks/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework import mixins
 
 from ebooks.models import Ebook, Review
-from ebooks.api.serializers import EbookSerializer
+from ebooks.api.serializers import EbookSerializer, ReviewSerializer
 
 class EbookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Ebook.objects.all()
@@ -13,6 +13,15 @@ class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
 
+
+class ReviewCreateAPIView(generics.CreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        ebook_pk = self.kwargs.get('ebook_pk')
+        ebook = get_object_or_404(Ebook, pk=ebook_pk)
+        serializer.save(ebook)
 
 # class EbookListCreateAPIView(mixins.ListModelMixin, 
 #                              mixins.CreateModelMixin, 
