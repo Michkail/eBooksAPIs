ac60fdd2e6cebaa12b1b9684d8ec593cf420b07f
michkail <pitermichkail@ymail.com>
2021-12-17 16:34:26 +0700

    test

---
+++ apiebooks/ebooks/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import (
+    Ebook,
+    Review
+)
+
+admin.site.register(Ebook)
+admin.site.register(Review)
---
+++ apiebooks/ebooks/api/views.py
@@ -0,0 +1,28 @@
+from rest_framework import generics
+from rest_framework import mixins
+
+from ebooks.models import Ebook, Review
+from ebooks.api.serializers import EbookSerializer
+
+class EbookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+# class EbookListCreateAPIView(mixins.ListModelMixin, 
+#                              mixins.CreateModelMixin, 
+#                              generics.GenericAPIView):
+
+#     queryset = Ebook.objects.all()
+#     serializer_class = EbookSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
